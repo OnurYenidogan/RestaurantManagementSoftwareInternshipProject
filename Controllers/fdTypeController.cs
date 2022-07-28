019ff6bedbc5b97db5e1fbde8da4c71d84297f3e
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2022-07-28 16:45:49 +0300

    FoodDrinkTypes Manager added

--- Controllers/fdTypeController.cs
+++ Controllers/fdTypeController.cs
@@ -7,12 +7,26 @@ using System.Web.Mvc;
 
 namespace MVCRestaurant27Tem2022.Controllers
 {
-    public class fdTypeController : Controller 
+    public class fdTypeController : Controller
     {
         RestaurantDBEntities db = new RestaurantDBEntities();
         public ActionResult Index()
         {
             return View(db.fdType.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(fdType newfdType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.fdType.Add(newfdType);
+                db.SaveChanges();
+            }
+            return View();
         }
     }
 }
