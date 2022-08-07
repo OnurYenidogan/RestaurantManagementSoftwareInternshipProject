d70fc981a513ae609802d26b7236469819c6e447
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2022-08-07 20:08:36 +0300

    Admin panel added

---
+++ Controllers/AdminPanelController.cs
@@ -0,0 +1,33 @@
+﻿using MVCRestaurant27Tem2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCRestaurant27Tem2022.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private RestaurantDBEntities db = new RestaurantDBEntities();
+        // GET: AdminPanel
+        public ActionResult Index(string submit)
+        {
+            switch (submit)
+            {
+                case "Table":
+                    return RedirectToAction("Table");
+
+                case "FD Type":
+                    return RedirectToAction("Index", "fdType");
+            }
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View(db.RTable.ToList());
+        }
+    }
+}
