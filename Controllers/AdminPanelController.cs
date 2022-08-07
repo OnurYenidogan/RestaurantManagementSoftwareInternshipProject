f741c8ba4211ac8ece7c49f431242c53e89e4370
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2022-08-07 21:22:36 +0300

    Remove and bring back tables with admin panel
    Admin Panel/Table
    -Admin can remove and bring back tables
    
    -"FreeTable" and "RemovedTable" pages are added
    
    -Only free tables can be removed
    
    Table Status
    
    -Removed tables won't be displayed on the "Table Status" page

--- Controllers/AdminPanelController.cs
+++ Controllers/AdminPanelController.cs
@@ -1,7 +1,9 @@
 ﻿using MVCRestaurant27Tem2022.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -31,6 +33,74 @@ namespace MVCRestaurant27Tem2022.Controllers
         public ActionResult Table()
         {
             return View(db.RTable.ToList());
+        }
+        public ActionResult FreeTable(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RTable rTable = db.RTable.Find(id);
+            if (rTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rTable);
+        }
+        // POST: RTable/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FreeTable(string submit, [Bind(Include = "id_rtable,tstatus")] RTable rTable)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (submit)
+                {
+                    case "Remove":
+                        rTable.tstatus = "x";
+                        db.Entry(rTable).State = EntityState.Modified;
+                        db.SaveChanges();
+                        return RedirectToAction("Table");
+                }
+
+            }
+            return View(rTable);
+        }
+        public ActionResult RemovedTable(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RTable rTable = db.RTable.Find(id);
+            if (rTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rTable);
+        }
+        // POST: RTable/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemovedTable(string submit, [Bind(Include = "id_rtable,tstatus")] RTable rTable)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (submit)
+                {
+                    case "Bring it back":
+                        rTable.tstatus = "f";
+                        db.Entry(rTable).State = EntityState.Modified;
+                        db.SaveChanges();
+                        return RedirectToAction("Table");
+                }
+
+            }
+            return View(rTable);
         }
     }
 }
